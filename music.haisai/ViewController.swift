e64ec3a724407659c9d9357503df091d974b4600
kei-iwai-koji <iw.kj.16@gmail.com>
2019-01-15 13:20:53 +0900

    Initial Commit

---
+++ music.haisai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  music.haisai
+//
+//  Created by 高司慶 on 2019/01/15.
+//  Copyright © 2019 kei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
